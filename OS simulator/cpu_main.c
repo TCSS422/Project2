32e950956951947338fa8b043c9b445de3895273
Peter <pentescu@uw.edu>
2014-03-09 20:08:24 -0700

    Instruction handlers and interrupt handlers should be roughed in

--- OS simulator/cpu_main.c
+++ OS simulator/cpu_main.c
@@ -13,9 +13,13 @@
 #include <unistd.h>
 #include <queue.h>
 
+#define TRUE 1
+#define FALSE 0
+
 #define RUN 1
 #define INTERRUPT 0
 
+// Note that keyboard is device 0
 #define NUM_DEVICES 2
 #define NUM_MUTEXES 3
 #define NUM_SHARED_MEM_LOCS 4
@@ -23,6 +27,20 @@
 
 #define NOBODY_HOLDS_MUTEX -1
 
+// possible interrupt state values
+// note: if more than one interrupt is received then these act as bitmasks
+// ex: both timer and io interrupts are received, then global_interrupt_state = 5
+// (TIMER_INTERRUPT + IO_INTERRUPT)
+// IO KEY TIMER
+// 1  0   1      = 5 (IO and timer)
+// 1  1   1      = 7 (all lit up)
+// 0  0   1      = 1 (just timer)
+#define UNINTERRUPTED 0
+#define TIMER_INTERRUPT 1
+#define KEYBOARD_INTERRUPT 2
+#define IO_INTERRUPT 4
+
+int global_interrupt_state;
 
 int main(int argc, char * argv[])
 {
@@ -31,6 +49,7 @@ int main(int argc, char * argv[])
 	ProcessStr * current_process;
 
 	PCBStr ** all_pcbs;
+	int run_scheduler = 0;
 
 	// Initialize blocked-process queue for devices
 	queue process_blocked_on_devices[NUM_DEVICES];
@@ -61,7 +80,7 @@ int main(int argc, char * argv[])
 
 		// sources of interrupts: timer, I/O devices, system calls
 
-		while (!INTERRUPT && current_pcb->state == RUNNING) {
+		while (global_interrupt_state == 0 && current_pcb->state == RUNNING) {
 			// increment pc
 			current_pcb->next_step = (current_pcb->next_step + 2) % current_process->no_steps;
 
@@ -141,6 +160,50 @@ int main(int argc, char * argv[])
 				printf("Illegal instruction\n");
 			}
 		}
+
 		// handle interrupts
+		// =================
+
+		// bitmask on timer interrupt
+		if (global_interrupt_state & TIMER_INTERRUPT)
+		{
+			global_interrupt_state -= TIMER_INTERRUPT;
+			printf("Timer interrupt received!");
+			// all we need to do is run the scheduler
+			run_scheduler = TRUE;
+		}
+
+		// bitmask on keyboard interrupt
+		if (global_interrupt_state & KEYBOARD_INTERRUPT)
+		{
+			global_interrupt_state -= KEYBOARD_INTERRUPT;
+			printf("Keyboard interrupt received!\n");
+			// Wake up the next process waiting on the keyboard
+			int waiting_process = getFirstItem(process_blocked_on_devices[0]);
+			// and wake it up... (note that we aren't immediately running the scheduler, it just is
+			// eligible to be scheduled!)
+			all_pcbs[waiting_process]->state = RUNNING;
+		}
+
+		// bitmask on IO interrupt
+		if (global_interrupt_state & IO_INTERRUPT)
+		{
+			global_interrupt_state -= IO_INTERRUPT;
+			printf("IO device interrupt received!\n")
+			// Find the next process waiting on the IO device
+			int waiting_process = getFirstItem(process_blocked_on_devices[1]);
+			// and wake it up...
+			all_pcbs[waiting_process]->state = RUNNING;
+		}
+
+		// See if we need to run the scheduler - either we just blocked on some system call,
+		// or we hit the timer interrupt.
+		if (run_scheduler || current_pcb->state == BLOCKED)
+		{
+			run_scheduler = FALSE;
+			// SCHEDULER MAGIC...
+			// current_pcb = newly_scheduled_pcb;
+			// current_process = newly_scheduled_process;
+		}
 	}
 }
